ace7fcb7aff3e13b28e3ecb4e8071a4ec6bc7f85
sotadaisuke <abighoop@gmail.com>
2019-08-30 02:53:17 +0000

    おみくじ6

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -26,8 +26,13 @@ class LinebotController < ApplicationController
          when Line::Bot::Event::Message
            case event.type
            when Line::Bot::Event::MessageType::Text
-             
-             
+             message = [
+               {
+                 type: "text",
+                 text: ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first + ".......だってさ！"
+               }
+             ]
+             client.reply_message(event["replyToken"], message)
            when Line::Bot::Event::MessageType::Location
              message = {
                type: "location",
@@ -38,14 +43,7 @@ class LinebotController < ApplicationController
              }
              client.reply_message(event["replyToken"], message)
            end
-           if event.message['text'] =~ /おみくじ/
-             message = [
-               {
-                 type: "text",
-                 text: ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first + ".......だってさ！"
-               }
-             ]
-            end
+          
             
          end
        }
